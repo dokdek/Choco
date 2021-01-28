5b2aa32e34e25f15ed69df1fa5a2d3e360b4dee3
dokdek <dokdek@gmail.com>
2021-01-28 17:43:44 -0500

    Added date property to kanji obj to fetch when reviews are due

--- helpers/new-user.js
+++ helpers/new-user.js
@@ -1,22 +1,36 @@
 const Discord = require("discord.js");
 const User = require("../models/user.model");
 
-const func = (message, kanjiArray) => {const newUser = new User({
-    userId: message.author.id,
-    level: 1,
-    toLearn: kanjiArray,
-  })
-  newUser.save()
-    .then(()=>{
+const func = (message, kanjiArray) => {
+  User.findOne({ userId: message.author.id }, (err, user) => {
+    if (err) {
+      message.reply("I encountered an error.");
+    } else if (user) {
       const embeddedGreeting = new Discord.MessageEmbed()
-      .setColor("#fd360b")
-      .setTitle("New User Dectected!")
-      .setDescription(
-        "It seems you are not in my database, I have created a profile for you. Good luck learning~!"
-      );
+        .setColor("#fd360b")
+        .setTitle("User already exists!")
+        .setDescription("You are already in my database!");
       message.reply(embeddedGreeting);
-    })
-    .catch((err)=>console.log(err));
-}
+    } else {
+      const newUser = new User({
+        userId: message.author.id,
+        level: 1,
+        toLearn: kanjiArray,
+      });
+      newUser
+        .save()
+        .then(() => {
+          const embeddedGreeting = new Discord.MessageEmbed()
+            .setColor("#fd360b")
+            .setTitle("New User Dectected!")
+            .setDescription(
+              "It seems you are not in my database, I have created a profile for you. Good luck learning~!"
+            );
+          message.reply(embeddedGreeting);
+        })
+        .catch((err) => console.log(err));
+    }
+  });
+};
 
 module.exports = func;
